77c4ea56df133f413c7197b598757706f8bbe00e
matt1-smith <mattwakeboard@gmail.com>
2022-12-20 21:04:21 -0500

    initial commit

---
+++ Cafe.py
@@ -0,0 +1,33 @@
+from Ingredients import MENU
+from Ingredients import resources
+
+def cafe_eshan():
+    #main function
+    place_order()
+
+
+
+# TODO user input for which beverage they'd like
+def place_order():
+    order = input("What would you like? (espresso/latte/cappuccino): ")
+    return order
+
+
+# TODO get info about specified drink
+# TODO tell how much the beverage costs
+# TODO check for ingredient level
+# TODO ask how many of each coin they insert
+# TODO receive payment
+# TODO brew the drink and decrease ingredients by proper amount
+# TODO able to print report of ingredient levels and money collected
+
+print("hi")
+
+
+# get user input
+# check ingredients
+# calculate costs
+# get payment
+# check payment
+# make the place_order()
+# transaction results
