09c33c44ca00a5ff197ba983c605f5e60dea1adf
matt1-smith <mattwakeboard@gmail.com>
2022-12-21 21:23:27 -0500

    add receive_payment function and sigfig to drink cost

--- Cafe.py
+++ Cafe.py
@@ -22,6 +22,28 @@ def check_levels(water_level, milk_level, coffee_level, req_water, req_milk, req
 
 
 # TODO ask how many of each coin they insert
+#receives payment and gives change or refunds if needed
+def receive_payment(order):
+    cost = MENU[order]['cost']
+    print(f"The {order} costs ${cost}")
+    print("Please insert coins")
+    quarters = int(input("How many quarters?: "))
+    dimes = int(input("How many dimes?: "))
+    nickels = int(input("How many nickels?: "))
+    pennies = int(input("How many pennies?: "))
+    total = quarters * .25 + dimes * .1 + nickels * .05 + pennies * .01
+    if total > cost:
+        change = total - cost
+        print(f"Your change is {change}.")
+        return True
+    elif total == cost:
+        print("Payment accepted.")
+        return True
+    else:
+        print(f"You did not insert enough money. Your refund is {total}")
+        return False
+
+
 # TODO receive payment
 # TODO brew the drink and decrease ingredients by proper amount
 # TODO able to print report of ingredient levels and money collected
@@ -41,5 +63,8 @@ def cafe_eshan():
     req_coffee = order_ingredients['coffee']
 
     levels_good = check_levels(water_level, milk_level, coffee_level, req_water, req_milk, req_coffee)
+    payment_good = receive_payment(user_order)
+
+    #if levels_good and payment_good:
 
 cafe_eshan()
